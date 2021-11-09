77d94fe7ef51dc3f3f359a4faa7954450331ca87
M. Caner Özkan <canerozkan513@gmail.com>
2021-11-09 14:23:31 +0300

    api

---
+++ Backend/SwapApp/SwapApp.API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SwapApp.Business.Abstract;
+using SwapApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwapApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController()
+        {
+
+        }
+
+        [HttpGet]
+        public List<User> Get()
+        {
+
+        }
+    }
+}
