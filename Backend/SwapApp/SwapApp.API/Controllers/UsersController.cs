ff245a1b078bfd2f1b0b4e3402b732effc850503
M. Caner Özkan <canerozkan513@gmail.com>
2021-11-11 22:40:31 +0300

    api updated

--- Backend/SwapApp/SwapApp.API/Controllers/UsersController.cs
+++ Backend/SwapApp/SwapApp.API/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SwapApp.Business.Abstract;
+using SwapApp.Business.Concrete;
 using SwapApp.Entities;
 using System;
 using System.Collections.Generic;
@@ -17,13 +19,37 @@ namespace SwapApp.API.Controllers
 
         public UsersController()
         {
-
+            _userService = new UserManager();
         }
 
         [HttpGet]
         public List<User> Get()
         {
+            return _userService.GetAllUsers();
+        }
 
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return _userService.GetUserById(id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] User user)
+        {
+            _userService.CreateUser(user);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] User user)
+        {
+            _userService.UpdateUser(user);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(User user)
+        {
+            _userService.DeleteUser(user);
         }
     }
 }
