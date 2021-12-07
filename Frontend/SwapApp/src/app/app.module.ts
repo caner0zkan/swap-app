0ce72753af39a2dc4283a95d855808c792c32f04
M. Caner Özkan <canerozkan513@gmail.com>
2021-12-07 18:19:00 +0300

    routing

--- Frontend/SwapApp/src/app/app.module.ts
+++ Frontend/SwapApp/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+
+
+const appRoutes: Routes= [
+  {path: "", component: HomepageComponent}, //localhost:4200
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
